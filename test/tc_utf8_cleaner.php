885d242341918051d65586a8e6322d8e7bacacef
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-01-07 10:54:32 +0100

    Test fixed

--- test/tc_utf8_cleaner.php
+++ test/tc_utf8_cleaner.php
@@ -8,7 +8,7 @@ class TcUtf8Cleaner extends TcBase {
 
 		$this->assertEquals("Příliš žluťoučký kůň úpěl ďábelské ódy",\Yarri\Utf8Cleaner::Clean("Příliš žluťoučký kůň úpěl ďábelské ódy"));
 		$this->assertEquals("_Příliš_ žl_uťoučký kůň úpěl ďábelské ódy_",\Yarri\Utf8Cleaner::Clean("{$invalid}Příliš{$invalid} žl{$invalid}uťoučký kůň úpěl ďábelské ódy{$invalid}"));
-		$this->assertEquals("?Příliš? žl_uťoučký kůň úpěl ďábelské ódy?",\Yarri\Utf8Cleaner::Clean("{$invalid}Příliš{$invalid} žl{$invalid}uťoučký kůň úpěl ďábelské ódy{$invalid}",array("replacement" => "?")));
+		$this->assertEquals("?Příliš? žl?uťoučký kůň úpěl ďábelské ódy?",\Yarri\Utf8Cleaner::Clean("{$invalid}Příliš{$invalid} žl{$invalid}uťoučký kůň úpěl ďábelské ódy{$invalid}",array("replacement" => "?")));
 		$this->assertEquals("▒Příliš▒ žl▒uťoučký kůň úpěl ďábelské ódy▒",\Yarri\Utf8Cleaner::Clean("{$invalid}Příliš{$invalid} žl{$invalid}uťoučký kůň úpěl ďábelské ódy{$invalid}","▒"));
 	}
 }
